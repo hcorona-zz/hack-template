3286cfeb001c23fa756dd43fe82a082043647ca7
hcorona <hcorona@192.168.0.101>
2014-03-23 22:32:29 +0000

    initial commit

---
+++ hack-template/src/main/java/model/Properties.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Properties {
+
+	private static String user 		= "";
+	private static String key 		= "";
+	private static String mxmKEY 	= ""; 
+	private static String echoKey 	= "";
+
+
+
+	public static  String getUser() {
+		return user;
+	}
+	public static String getKey() {
+		return key;
+	}
+	public static String getMxmKEY() {
+		return mxmKEY;
+	}
+	public static String getEchoKey() {
+		return echoKey;
+	}
+
+
+
+}
